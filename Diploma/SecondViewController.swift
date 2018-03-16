d09750a38ed790dc1fc2973ac8de285f4e8262d7
uecm <uecmegor@gmail.com>
2018-03-16 11:08:14 +0200

    Initial Commit

---
+++ Diploma/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Diploma
+//
+//  Created by Egor on 3/16/18.
+//  Copyright © 2018 egor. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
